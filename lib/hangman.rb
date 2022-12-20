476cc9f1f8100d2aa3b83e2b27882fd01502e4a4
Joel Howard <joelhoward2@gmail.com>
2022-12-19 23:57:14 -0500

    add dictionary class and empty game class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,24 @@
-dictionary = File.open('google-10000-english-no-swears.txt')
-contents = dictionary.readlines
-dictionary.close
-random_word = contents.filter_map { |el| el.strip if el.strip.length.between?(5, 12) }.sample
+class Game
+
+end
+
+class Dictionary
+  def initialize(contents = "placeholder")
+    @contents = "placeholder"
+  end
+
+  def load_dictionary
+    dictionary = File.open('google-10000-english-no-swears.txt')
+    @contents = dictionary.readlines
+    dictionary.close
+  end
+
+  def select_word
+    random_word = @contents.filter_map { |el| el.strip if el.strip.length.between?(5, 12) }.sample
+    puts random_word
+  end
+end
+
+dictionary = Dictionary.new
+dictionary.load_dictionary
+dictionary.select_word
