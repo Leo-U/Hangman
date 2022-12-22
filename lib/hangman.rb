aae675a9bbed22a85d4b05c54dcad6f2913f02c0
Joel Howard <joelhoward2@gmail.com>
2022-12-22 04:23:15 -0500

    add play_game method to Game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,18 +1,13 @@
 class Game
-  
-  attr_reader :correct_letters, :incorrect_letters
-  def del_me
-    @correct_letters = []
-  end
-
-  def initialize_game
+  def initialize
     dictionary = Dictionary.new
     dictionary.load_dictionary('google-10000-english-no-swears.txt')
     @secret_word = dictionary.select_word
 
-    @guesses_left = 7
+    @mistakes_left = 7
     @correct_letters = []
     @incorrect_letters = []
+    @display_str = ''
   end
 
   def input_guess (guess)
@@ -29,31 +24,31 @@ class Game
 
   def display_game
     secret = @secret_word.split ''
-    puts @secret_word
-    display_str = ''
     secret.each_with_index do |el, i|
       if @correct_letters.include?(el)
-        display_str += el + ' '
+        @display_str += el + ' '
       else
-        display_str += '_ '
+        @display_str += '_ '
       end
     end
-    puts display_str
+    puts @display_str
   end
 
+  def play_game
+    until @mistakes_left == 0 do
+      self.input_guess(gets.chomp)
+      self.check_letter
+      puts "Correct letters: #{@correct_letters * ','}"
+      puts "Incorrect letters: #{@incorrect_letters * ','}"
+      self.display_game
 
-
-    #needs to display correct and incorrect letters. just print to console.
-    #also print remaining guesses.
-
-    #1. display remaining guesses
-    #2. display word with underscores and spaces
-      #A) display spaced underscore sequence with length of word. 
-      #B) replace corresponding underscores with correctly guessed letters
-    
-    
-
-
+      @mistakes_left -= 1 if @incorrect_letters.include?(@guess)
+      puts "Mistakes left: #{@mistakes_left}"
+      puts @secret_word if @mistakes_left == 0
+      if 
+      end
+    end
+  end
 end
 
 class Dictionary
@@ -72,16 +67,6 @@ class Dictionary
     random_word
   end
 end
+
 game = Game.new
-game.del_me
-
-
-game.initialize_game
-50.times do
-game.input_guess(gets.chomp)
-game.check_letter
-p game.correct_letters
-p game.incorrect_letters
-game.display_game
-
-end
+game.play_game
